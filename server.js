4b273f20119fc2c9bd18405c3cf82d39550d4973
projets-pe <projets-pe@sycorax.fr>
2020-08-14 17:57:21 +0200

    server.js update

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-//Install express server
 const express = require('express');
+const http = require('http')
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/covid19'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/covid19/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.route('/*', function(req,res) {
+  res.redirect(__dirname + '/dist/index.html')
+})
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
