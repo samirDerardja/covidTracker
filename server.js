14ed6e942ba7f47b31d3f13b15973bb97ee2e21b
projets-pe <projets-pe@sycorax.fr>
2020-08-15 15:47:19 +0200

    update heroku

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.configure(function () {
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(allowCrossDomain);
-  app.use(express.static(__dirname + '/dist/covidTracker'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
 
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/covidTracker'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/covidTracker/index.html'));
@@ -29,15 +24,15 @@ const server = http.createServer(app);
 server.listen(port, () => console.log('running'));
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 };
