bb6855e47c58c6f8941313197e5a274a5ff27503
projets-pe <projets-pe@sycorax.fr>
2020-08-15 14:23:20 +0200

    build update

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/covidTracker'));
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(allowCrossDomain);
+  app.use(express.static(__dirname + '/dist/covidTracker'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/covidTracker/index.html'));
@@ -20,3 +27,17 @@ app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('running'));
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
